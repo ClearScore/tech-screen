3d74763437f55a6ff8b628491a6c62f977c30816
daniel jenkins <dan@daniel-jenkins.com>
2018-10-07 22:35:57 +0200

    Enable server to wait for async data before rendering response
    - Added `getData` to routes config
    - Enable SSR processing of async action & injecting data onto the window object before render completes
    - Moved react-router to new client mounting method 'hydrate'

--- carousel/src/server/templates/Html.jsx
+++ carousel/src/server/templates/Html.jsx
@@ -5,7 +5,7 @@ import DocumentMeta from 'react-document-meta';
 import { hasWindow } from '../../app/utils';
 
 const Html = ({
-  js, css, markup,
+  js, css, markup, data
 }) => (
   <html lang="en">
     <head>
@@ -18,6 +18,7 @@ const Html = ({
       <div id="stylesheets" dangerouslySetInnerHTML={{ __html: css.join('') }} />
       <div id="html" dangerouslySetInnerHTML={{ __html: markup }} />
       <div id="scripts" dangerouslySetInnerHTML={{ __html: js.join('') }} />
+      <script type="text/javascript" dangerouslySetInnerHTML={{__html: `window.__INITIAL_DATA__ = ${JSON.stringify(data)}`}}></script>
     </body>
   </html>
 );
