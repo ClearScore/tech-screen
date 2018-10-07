97f2792d0d10bc8d774ba2385034a10bd84beda4
daniel jenkins <dan@daniel-jenkins.com>
2018-10-07 22:37:08 +0200

    Add getCreditReport action to dashboard component
    Dashboard.jsx (client) expects data on componentDidMount from server.

--- carousel/src/app/components/Dashboard/Dashboard.jsx
+++ carousel/src/app/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./dashboard.scss";
+import { hasWindow } from "../../utils";
+class Dashboard extends React.Component {
+  state = {
+    creditReport: []
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState(
+        {
+          creditReport: hasWindow ? window.__INITIAL_DATA__ || [] : []
+        },
+        state => {
+          console.log(this.state);
+        }
+      );
+    }, 0);
+  }
 
-import './dashboard.scss';
+  render() {
+    const { creditReportInfo } = this.state.creditReport;
+    return <div>{creditReportInfo && creditReportInfo.score}</div>;
+  }
+}
 
-export default () => (
-  <div >
-    Hello World!!
-  </div>
-);
+export default Dashboard;
