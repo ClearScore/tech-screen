9cec0923bc6f0036843d598d8bc44da98870d66d
ontz <serban.ontanucraciun@gmail.com>
2019-04-17 00:23:20 +0100

    Add score bar with animation

--- carousel/src/app/components/CreditScore/CreditScore.jsx
+++ carousel/src/app/components/CreditScore/CreditScore.jsx
@@ -7,18 +7,35 @@ import './creditScore.scss';
 
 const cn = bemHelper({ block: 'creditscore' });
 
-const CreditScore = (props) => (
-  <div className={cn(null, 'main')}>
-    Your credit score is
-    <div className={cn('score')}>
-      {props.score}
+const CreditScore = (props) => {
+  const dashArray = 250;
+  let dashOffset = 250;
+  if (props.max !== 0) {
+    dashOffset = dashOffset - dashOffset * (Math.abs(props.score) / Math.abs(props.max));
+  }
+  return (
+    <div className={cn(null, 'main')}>
+      <div className={cn('progress-bar')}>
+        <svg viewBox='0 0 100 100'>
+          <circle cx='50' cy='50' r='48'
+            fill='transparent'
+            stroke-width='1'
+            stroke-dasharray={dashArray}
+            stroke-dashoffset={dashOffset}
+          />
+        </svg>
+      </div>
+      Your credit score is
+      <div className={cn('score')}>
+        {props.score}
+      </div>
+      out of <span className={cn('max')}>{props.max}</span>
+      <div className={cn('band-description')}>
+        {props.bandDescription}
+      </div>
     </div>
-    out of <span className={cn('max')}>{props.max}</span>
-    <div className={cn('band-description')}>
-      {props.bandDescription}
-    </div>
-  </div>
-);
+  )
+};
 
 CreditScore.propTypes = {
   score: PropTypes.number.isRequired,
